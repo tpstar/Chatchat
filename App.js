aceae609d940a5858bf054f5a48a2b1e315b465d
tpstar <hanlee12@gmail.com>
2018-06-29 10:18:53 -0500

    start Expo chat app

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import io from 'socket.io-client/dist/socket.io';
+
+export default class App extends React.Component {
+  state = {
+    name: 'Bob'
+  }
+
+  constructor() {
+    super();
+
+    this.socket = io('localhost:3000', {jsonp: false})
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.name}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
